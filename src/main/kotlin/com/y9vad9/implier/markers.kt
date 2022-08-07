d1d7c5c31e36e7e071b8362f9da75a6deb7133aa
Vadim Yaroschuk <y9vad9@gmail.com>
2022-08-07 18:28:19 +0300

    fix: codestyle fix

--- src/main/kotlin/com/y9vad9/implier/markers.kt
+++ src/main/kotlin/com/y9vad9/implier/markers.kt
@@ -1,6 +1,6 @@
 package com.y9vad9.implier
 
 interface GenericMarker<T>
-interface Mutable<T>: GenericMarker<T>
-interface Immutable<T>: GenericMarker<T>
-interface Dto<T>: GenericMarker<T>
+interface Mutable<T> : GenericMarker<T>
+interface Immutable<T> : GenericMarker<T>
+interface Dto<T> : GenericMarker<T>
